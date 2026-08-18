2a35ea8f2f568be20998f4a11c478cabce603632
Rikuga <pranatayudhapratama20@gmail.com>
2026-08-18 08:31:54 +0700

    fix: static page to dynamic page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { prisma } from "@/lib/prisma";
 import LogoutButton from "@/components/admin/LogoutButton";
 
+export const dynamic = "force-dynamic";
+
 export default async function AdminDashboard() {
     const [techStackCount, projectCount] = await Promise.all([
         prisma.techStack.count(),
